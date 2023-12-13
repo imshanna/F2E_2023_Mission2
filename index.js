eca043576a8317eb0586829898c1da316214abd1
imshanna <jsk60419@gmail.com>
2023-12-13 22:14:35 +0800

    Updates

---
+++ index.js
@@ -0,0 +1,115 @@
+import { yearList, getCityList, getDistList, set } from "./section/utils.js"
+import { loadPage, loadCandList } from "./section/main.js"
+import { drawBarChart, drawLineChart } from "./section/chart.js"
+import { drawMap } from "./section/map.js"
+
+const yearOptions = document.querySelector(".year-select .options");
+const cityOptions = document.querySelector(".city-select .options");
+const distOptions = document.querySelector(".dist-select .options");
+
+const yearSelected = document.getElementById("year");
+const citySelected = document.getElementById("city");
+const distSelected = document.getElementById("dist");
+
+function addOptions(list, target){
+    list.forEach((item) => {
+        const li = document.createElement("li");
+        li.setAttribute("data-value", item);
+        li.innerHTML = item;
+        target.appendChild(li);
+    })
+}
+
+addOptions(yearList, yearOptions);
+
+const observerYear = new MutationObserver((mutationsList, observer) => {
+    mutationsList.forEach((mutation) => {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'data-value') {
+            const currentYear = mutation.target.getAttribute('data-value');
+            yearSelected.innerHTML = currentYear;
+            set.year(currentYear);
+
+            const cityList = getCityList();
+            cityList.unshift("全國")
+            citySelected.dataset.value = cityList[0];
+
+            cityOptions.innerHTML = "";
+            addOptions(cityList, cityOptions);
+
+            loadCandList();
+            drawMap();
+        }
+    });
+});
+
+const observerCity = new MutationObserver((mutationsList, observer) => {
+    mutationsList.forEach((mutation) => {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'data-value') {
+            const currentCity = mutation.target.getAttribute('data-value');
+            citySelected.innerHTML = currentCity;
+            set.city(currentCity);
+
+            const distList = (currentCity === "全國") ? [] : getDistList();
+            distList.unshift("全部區域")
+            distSelected.dataset.value = distList[0];
+
+            distOptions.innerHTML = "";
+            addOptions(distList, distOptions);
+        }
+    });
+});
+
+const observerDist = new MutationObserver((mutationsList, observer) => {
+    mutationsList.forEach((mutation) => {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'data-value') {
+            const currentDist = mutation.target.getAttribute('data-value');
+            distSelected.innerHTML = currentDist;
+            set.dist(currentDist);
+            
+            loadPage();
+        }
+    });
+});
+
+const config = { attributes: true, childList: false, subtree: false };
+
+observerYear.observe(yearSelected, config);
+observerCity.observe(citySelected, config);
+observerDist.observe(distSelected, config);
+
+// Dropdown Menu
+
+const select = document.querySelectorAll(".select");
+
+select.forEach((selectItem) => {
+    const selected =  selectItem.querySelector(".selected");
+    const optionList = selectItem.querySelector(".options");
+
+    selectItem.addEventListener("click", (e) => {
+        e.stopPropagation(); // prevent bubbling to document event listener
+
+        const selectActive = document.querySelector(".active");
+        if(selectActive && selectActive !== selectItem) {  // only one selectItem can be displayed at a time
+            selectActive.classList.remove("active");
+        }
+
+        selectItem.classList.toggle("active");
+    })
+
+    optionList.addEventListener("click", (e) => {
+        if(e.target.tagName.toLowerCase() === "li"){
+            selected.dataset.value = e.target.dataset.value;
+        }
+    })
+})
+
+document.addEventListener("click", () => {
+    const selectActive = document.querySelector(".active");
+    selectActive && selectActive.classList.remove("active");
+})
+
+// Init value
+yearSelected.dataset.value = "2020";
+
+drawBarChart();
+drawLineChart();
---
+++ section/utils.js
@@ -0,0 +1,88 @@
+import voteData from "../data/election.json" assert { type: "json" };
+import candData from "../data/candidates.json" assert { type: "json" };
+
+let currentYear, currentCity, currentDist;
+
+export const set = {
+    year: function(year){
+        currentYear = year;
+    },
+    city: function(city){
+        currentCity = city;
+    },
+    dist: function(dist){
+        currentDist = dist;
+    }
+}
+
+export const get = {
+    year: function(){
+        return currentYear;
+    },
+    city: function(){
+        return currentCity;
+    },
+    dist: function(){
+        return currentDist;
+    }
+}
+
+export const link = {
+    toYear: function(year){
+        document.getElementById("year").dataset.value = year;
+    },
+    toCity: function(city){
+        document.getElementById("city").dataset.value = city;
+    },
+    toDist: function(dist){
+        document.getElementById("dist").dataset.value = dist;
+    }
+}
+
+export function maxParty(){
+    let prev = 0;
+    let max = "";
+
+    return function(num, party){
+        max = num > prev ? party : max;
+        prev = num;
+        return max;
+    }
+}
+
+export const yearList = Object.keys(voteData);
+
+export function getCityList(){
+    return Object.keys(voteData[currentYear].縣市)
+}
+
+export function getDistList(){
+    return Object.keys(voteData[currentYear].縣市[currentCity].鄉鎮市區)
+}
+
+export function getVoteData(){
+    if((currentCity === "全國") || !currentCity){
+        return voteData[currentYear];
+
+    }else if((currentCity != "全國") && (currentDist === "全部區域")){
+        return voteData[currentYear].縣市[currentCity];
+
+    }else if(currentDist != "全部區域"){
+        return voteData[currentYear].縣市[currentCity].鄉鎮市區[currentDist];
+    }
+}
+
+export function getCandData(){
+    return candData[currentYear];
+}
+
+export const partyColor = {
+    "國民黨": "#8082FF",
+    "民進黨": "#57D2A9",
+    "無黨籍": "#CDCDCD",
+    "親民黨": "#F4A76F",
+    "新黨": "#F4D625",
+    "其他": "#9D9D9D",
+    "連署(1)": "#CDCDCD",
+    "連署(4)": "#986769"
+}
